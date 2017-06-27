552259ffc60167c35a3c4ee079ffdacffcbbd4d8
fozzedout <fozzedout@gmail.com>
2017-06-27 21:55:48 +0100

    githubs connection to call a remote pull

--- ghpull.php
+++ ghpull.php
@@ -1,1 +1,9 @@
-<?php 'git pull';?>
+<?php 
+ error_reporting(E_ALL);
+ ini_set('display_errors', 1);
+
+while(ob_get_level())ob_end_clean(); // remove output buffers
+ob_implicit_flush(true);             // output stuff directly
+
+echo shell_exec('"C:\Program Files\Git\bin\git.exe" pull 2>&1');
+?>
